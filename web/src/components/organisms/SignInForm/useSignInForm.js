1339e965bcdc579a2d944a30b97ad25ce75199f9
Hilarión <50677192+Darkboy5120@users.noreply.github.com>
2022-02-01 02:12:02 -0600

    implementada validacion de email

--- web/src/components/organisms/SignInForm/useSignInForm.js
+++ web/src/components/organisms/SignInForm/useSignInForm.js
@@ -2,7 +2,7 @@ import useFormField from "../../../assets/hooks/useFormField";
 import inputValidations from "../../../assets/controllers/inputValidations";
 
 const useSignInForm = () => {
-  const email = useFormField(inputValidations.string);
+  const email = useFormField(inputValidations.email);
   const password = useFormField(inputValidations.string);
 
   const state = {email, password};
