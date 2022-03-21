c08374396e78e0788601f75f03fda126c181bd50
Hilarión <50677192+Darkboy5120@users.noreply.github.com>
2022-03-21 12:18:03 -0600

    added loading spinner to loadCategories component

--- web/src/components/molecules/LoadCategories/index.js
+++ web/src/components/molecules/LoadCategories/index.js
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import CategotyItem from '../CategoryItem';
 import request from '../../../assets/controllers/request';
+import LoadingSpinner from '../../atoms/LoadingSpinner';
 import styles from './styles.module.css';
 
 const getCategories = (setCategories) => {
@@ -20,9 +21,9 @@ function LoadCategories() {
 
   return (
     <div className={styles.container}>
-      {categories?.map((category) => (
+      {categories ? categories?.map((category) => (
         <CategotyItem key={category.pk_id} name={category.name} id={category.pk_id} />
-      ))}
+      )) : <LoadingSpinner size="big" /> }
     </div>
   );
 }
