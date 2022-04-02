6006727fd6b60a2d09954b649870975209ec196c
Hilarión <50677192+Darkboy5120@users.noreply.github.com>
2022-04-02 16:14:27 -0600

    prepared component props to add ar actions functionalitues

--- web/src/components/atoms/AppStats/index.js
+++ web/src/components/atoms/AppStats/index.js
@@ -3,12 +3,24 @@ import { FaHeart, FaThumbsUp, FaDownload } from 'react-icons/fa';
 import FlexContainer from '../../../layouts/FlexContainer';
 import IconNumber from '../IconNumber';
 
-function AppStats({ appDetails }) {
+function AppStats({ appDetails, allowButtons }) {
+  let onClicks = {
+    favorites: null,
+    popularity: null,
+    downloads: null,
+  };
+  if (allowButtons) {
+    onClicks = {
+      favorites: () => console.log(1),
+      popularity: () => console.log(2),
+      downloads: () => console.log(3),
+    };
+  }
   return (
     <FlexContainer>
-      <IconNumber icon={<FaHeart />} number={appDetails.favorites} />
-      <IconNumber icon={<FaThumbsUp />} number={appDetails.endorsements} />
-      <IconNumber icon={<FaDownload />} number={appDetails.downloads} />
+      <IconNumber icon={<FaHeart />} number={appDetails.favorites} onClick={onClicks.favorites} />
+      <IconNumber icon={<FaThumbsUp />} number={appDetails.popularity} onClick={onClicks.popularity} />
+      <IconNumber icon={<FaDownload />} number={appDetails.downloads} onClick={onClicks.downloads} />
     </FlexContainer>
   );
 }
--- web/src/components/atoms/IconNumber/index.js
+++ web/src/components/atoms/IconNumber/index.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import FlexContainer from '../../../layouts/FlexContainer';
 import CustomText from '../CustomText';
+import CustomButton from '../CustomButton';
 import styles from './styles.module.css';
 
-function IconNumber({ icon, number }) {
-  return (
+function IconNumber({ icon, number, onClick }) {
+  return onClick ? (
+    <CustomButton title={`${number}`} leftIcon={icon} className={styles.container} onClick={onClick} />
+  ) : (
     <FlexContainer hCentered vCentered className={styles.container}>
       {icon}
       <CustomText className={styles.text}>{number}</CustomText>
