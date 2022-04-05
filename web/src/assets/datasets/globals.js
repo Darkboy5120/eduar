27670f8eee64a658800c90a339270424ff87fbde
Hilarión <50677192+Darkboy5120@users.noreply.github.com>
2022-04-04 19:00:27 -0500

    setup deploy config

--- web/src/assets/datasets/globals.js
+++ web/src/assets/datasets/globals.js
@@ -1,7 +1,7 @@
 const globals = {
   server: {
-    path: 'http://localhost:3001/',
-    // path: 'https://eduar-server.herokuapp.com/',
+    // path: 'http://localhost:3001/',
+    path: 'https://eduar-server.herokuapp.com/',
   },
   local: {
     path: 'http://localhost:3000/',
